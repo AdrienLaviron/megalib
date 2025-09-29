672acd8c3ff02afa89df1109f901d43dc49e2216
Adrien Laviron <adrien.laviron@nasa.gov>
2025-09-29 11:41:45 -0400

    Feat: First attempt at compiling with onnxruntime

---
+++ src/revan/inc/MEREventTypeOnnx.h
@@ -0,0 +1,71 @@
+/*
+ * MEREventTypeOnnx.h
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ * Please see the source-file for the copyright-notice.
+ *
+ */
+
+
+#ifndef __MEREventTypeOnnx__
+#define __MEREventTypeOnnx__
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// ROOT libs:
+#include <TROOT.h>
+
+// MEGAlib libs:
+#include "MGlobal.h"
+#include "MEREventType.h"
+#include "MFileEventsType.h"
+#include "MRawEventIncarnations.h"
+
+// Onnx libs:
+#include <onnxruntime_cxx_api.h>
+
+// Forward declarations:
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+class MEREventTypeOnnx : public MEREventType
+{
+  // public interface:
+ public:
+  MEREventTypeOnnx();
+  virtual ~MEREventTypeOnnx();
+
+  //! Global parameters used by all electron tracking algorithms
+  virtual void SetParameters(MString EventTypeFileName);
+  bool Analyze(MRawEventIncarnations* List);
+
+  bool PostAnalysis();
+  MString ToString(bool CoreOnly = false) const;
+
+
+  // protected members:
+ protected:
+  MString m_EventTypeFileName;
+  MFileEventsType* m_FileEventsType;
+
+  //torch::jit::script::Module m_module;
+
+
+#ifdef ___CLING___
+ public:
+  ClassDef(MEREventTypeOnnx, 0) // no description
+#endif
+
+};
+
+#endif
+
+
+
+
---
+++ src/revan/src/MEREventTypeOnnx.cxx
@@ -0,0 +1,130 @@
+/*
+ * MEREventTypeOnnx.cxx
+ *
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+
+// Include the header:
+#include "MEREventTypeOnnx.h"
+
+
+// MEGAlib libs:
+#include "MAssert.h"
+#include "MFileEventsType.h"
+
+// Onnx libs:
+#include <onnxruntime_cxx_api.h>
+
+// C++ std libs:
+#include <iostream>
+#include <memory>
+#include <vector>
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifdef ___CLING___
+ClassImp(MEREventTypeOnnx)
+#endif
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+MEREventTypeOnnx::MEREventTypeOnnx() : MEREventType()
+{
+  m_EventTypeFileName = "";
+  m_FileEventsType = new MFileEventsType();
+}
+
+MEREventTypeOnnx::~MEREventTypeOnnx()
+{
+  delete m_FileEventsType;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+void MEREventTypeOnnx::SetParameters(MString EventTypeFileName)
+{
+/*  m_EventTypeFileName = EventTypeFileName;
+  try {
+    m_module = torch::jit::load(m_EventTypeFileName);
+    m_module.eval();
+    mout << "Module " << m_EventTypeFileName << " loaded successfully." << endl;
+  } catch (const c10::Error& e) {
+    merr << "Error loading model: " << e.msg() << endl;
+  }*/
+}
+
+bool MEREventTypeOnnx::PostAnalysis()
+{
+  return m_FileEventsType->Close();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool MEREventTypeOnnx::Analyze(MRawEventIncarnations* List)
+{
+  MERConstruction::Analyze(List);
+
+  if (! m_FileEventsType->IsOpen() ) {//First event
+    m_FileEventsType->Open(m_EventTypeFileName); //Read-mode
+  }
+
+  // Read file
+  streampos filePos = m_FileEventsType->GetFilePosition(); // Position at start of REI
+  MRERawEvent* RE = nullptr;  
+  for (int e = 0; e < m_List->GetNRawEvents(); e++) {
+    m_FileEventsType->Seek(filePos);// Rewind to position at start of REI
+    RE = m_List->GetRawEventAt(e);
+    while(m_FileEventsType->GetNextEvent() && m_FileEventsType->GetEventId() != RE->GetEventId()) {}
+    mout << "evtid=" << m_FileEventsType->GetEventId() << endl;
+    if (m_FileEventsType->GetEventId() == RE->GetEventId()) { // if found matching ID
+      RE->SetEventType( m_FileEventsType->GetEventType() );
+      RE->SetEventTypeProbability( m_FileEventsType->GetEventTypeProbability() );
+    } else {
+      merr << "MEREventTypeOnnx: No event type found for event ID " << RE->GetEventId() << endl;
+      RE->SetEventType( c_UnknownEvent );
+      RE->SetEventTypeProbability( 0. );
+    }
+  }
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+MString MEREventTypeOnnx::ToString(bool CoreOnly) const
+{
+  // Dump an options string gor the tra file:
+  ostringstream out;
+
+  if (CoreOnly == false) {
+    out<<"# Event type - options:"<<endl;
+    out<<"# "<<endl;
+  }
+  out<<"# Event Type file name:         "<<m_EventTypeFileName<<endl;
+  if (CoreOnly == false) {
+    out<<"# "<<endl;
+  }
+
+  return out.str().c_str();
+}
+
